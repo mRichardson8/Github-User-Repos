2cc9f773e752d7c2e946d9cad68e6e5e7002eea4
Matthew Richardson <matt.rich2045@gmail.com>
2022-08-10 10:36:30 +0100

    Created base jsx for app and added axios request for github

--- github-user-repos/src/App.js
+++ github-user-repos/src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import env from "react-dotenv";
 function App() {
+  const [username, setUsername] = useState('')
+  const [timer, setTimer] = useState()
+  const [gitData, setGitData] = useState()
+  const handleChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    makeRequest()
+  }
+
+  // useEffect(()=>{
+  //   const makeRequest = async () => {
+  //     try{
+  //       console.log(env.API_KEY)
+  //       const { data } = await axios.get(`https://api.github.com/users/${username}/repos`, {headers: {"Authorization" : env.API_KEY}})
+
+  //     }catch(err){
+
+  //     }
+  //   }
+  // }, [username])
+
+  const makeRequest = async () => {
+        try{
+          const { data } = await axios.get(`https://api.github.com/users/${username}/repos`, {headers: {"Authorization" : window.env.API_KEY}})
+          setGitData(data)
+        }catch(err){
+          console.log("big error", err.message)
+        }
+      }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={handleChange} placeholder='Enter your username'/>
+          </form>
+      </div>
+      <div className="data-cont">
+        {gitData ? <>
+          <section>You have {gitData.length} Repos</section>
+          {gitData.map(repo => {
+            //Add Repo component for each repo that displays the relevant data
+          })}
+        </> : <section>Error, you are not a user</section>}
+      </div>
     </div>
   );
 }
